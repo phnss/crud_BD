36bb33c1b8353fa91b7e91b8fec45079fd7eb308
Diego Reis <diegoluis200302@gmail.com>
2023-05-04 12:15:05 -0300

    Update controller.js

--- controller.js
+++ controller.js
@@ -3,53 +3,75 @@ const Client = require('pg').Client
 const cliente = new Client(DBconfigs);
 
 //----- FUNÇÕES ASSÍNCRONAS ------
-getProdutos();
-//insertProduto(3, 'maçã', 1.00, 1);
-//deleteProduto(3);
+start();
 
+async function start()
+{
+    try 
+    {
+        await connect();
+        //await deleteProduto(0);
+        await insertProduto(0, 'maçã', 1.00, 1);
+        await insertProduto(1, 'laranja', 1.00, 1);
+        await getProdutos();
+        await disconnect();
+    }
+    catch(e) 
+    {
+        console.log("Ocorreu erro. "+ex)   
+    } 
+}
 
-async function getProdutos(){
+async function connect()
+{
     try{
         console.log("iniciando a conexão.");
-        await cliente.connect();                                        //inicia a conexão
-        console.log("Conexão bem sucedida!");                           
+        await cliente.connect(); //inicia a conexão
+        console.log("Conexão bem sucedida!");
+    }
+    catch(ex){
+        console.log("Ocorreu erro ao conectar cliente. "+ex)    
+    }
+}
+
+async function disconnect()
+{
+    try{
+        console.log("iniciando a desconexão.");
+        await cliente.end();
+        console.log("Desconectado com sucesso.");
+    }
+    catch(ex){
+        console.log("Ocorreu erro ao desconectar cliente. "+ex)    
+    }
+}
+
+async function getProdutos()
+{
+    try{                         
         const resultado = await cliente.query("SELECT * FROM produtos"); //executa a querry SQL
         console.table(resultado.rows);                                 //Lista as tabela no terminal
     }
     catch(ex){
         console.log("Ocorreu erro no getProdutos. "+ex)    
     }
-    finally{
-        await cliente.end();
-        console.log("Cliente desconectado!");
-    }
 }
 
-async function insertProduto(cod, nome, preco, qtd){
+async function insertProduto(cod, nome, preco, qtd)
+{
     try{
-        console.log("iniciando a conexão.");
-        await cliente.connect();
-        console.log("Conexão bem sucedida!");
         await cliente.query('INSERT INTO produtos("cod", "nome", "preço", "quantidade") values('+"'"+cod+"','"+nome+"','"+preco+"','"+qtd+"');");
         console.log("Valor inserido na tabela!");
-
         const resultado = await cliente.query("SELECT * FROM produtos");
         console.table(resultado.rows);
     }
     catch(ex){
         console.log("Ocorreu erro no insertProduto. "+ex)    
-    }
-    finally{
-        await cliente.end();
-        console.log("Cliente desconectado!");
-    }    
+    }   
 }
 
 async function deleteProduto(cod){
     try{
-        console.log("iniciando a conexão.");
-        await cliente.connect();
-        console.log("Conexão bem sucedida!");
         await cliente.query("DELETE FROM produtos WHERE cod = '"+cod+"';");
         console.log("Produto removido da tabela na tabela!");
 
@@ -57,10 +79,6 @@ async function deleteProduto(cod){
         console.table(resultado.rows);
     }
     catch(ex){
-        console.log("Ocorreu erro no insertProduto. "+ex)    
-    }
-    finally{
-        await cliente.end();
-        console.log("Cliente desconectado!");
-    }    
+        console.log("Ocorreu erro no deleteProduto. "+ex)    
+    } 
 }
